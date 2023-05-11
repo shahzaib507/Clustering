94cbb4cd94306a181377016b90fcd8976b2c6e09
shahzaib507 <102601162+shahzaib507@users.noreply.github.com>
2023-05-11 20:45:17 +0500

    Update Shahzaib.py
    It is final version

--- Shahzaib.py
+++ Shahzaib.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import itertools as iter
+import itertools as  iter
 import scipy.optimize as opt
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score as silhouete_score
